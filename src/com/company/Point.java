61a15c1c6e0956057dbc713f9d993c04fac0a86e
Josué Yao <jyao2000@yahoo.fr>
2020-10-01 08:52:26 +0000

    finish constructor

--- src/com/company/Point.java
+++ src/com/company/Point.java
@@ -3,8 +3,9 @@ package com.company;
 public class Point {
     private int x;
     private int y;
-    
-    public Point(int x, int y){
 
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
     }
 }
