bf62c9e40c2e8dfaed80bb2457d8ef61f3fe4013
Josué Yao <jyao2000@yahoo.fr>
2020-10-01 09:05:57 +0000

    create Afficher

--- src/com/company/Segment.java
+++ src/com/company/Segment.java
@@ -9,4 +9,9 @@ public class Segment {
         this.ext1 = ext1;
         this.ext2 = ext2;
     }
+
+    public void Afficher(){
+        this.ext1.Afficher();
+        this.ext2.Afficher();
+    }
 }
